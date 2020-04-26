b73e5346b272f3ae22d3ac172688b170cb0663b7
Saishashidhar66 <53369848+Saishashidhar66@users.noreply.github.com>
2020-04-26 16:15:43 +0530

    Update trainingcatsvsdogs.py

--- trainingcatsvsdogs.py
+++ trainingcatsvsdogs.py
@@ -2,25 +2,17 @@ import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense,Dropout,Activation,Flatten
 import pickle
-
 from keras.layers import Conv2D,MaxPooling2D
 
 pickle_in=open(r"D:\shashi\py\Features.pickle","rb")
 X=pickle.load(pickle_in)
-
 pickle_in=open(r"D:\shashi\py\Labels.pickle","rb")
 y=pickle.load(pickle_in)
 
 print(X)
-
-
-
 X=X/255.0
-
 print(X)
-
 model= Sequential()
-
 model.add(Conv2D(256,(3,3),input_shape=X.shape[1:]))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
@@ -32,16 +24,10 @@ model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
 model.add(Dense(64))
 
-
-
-
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
-
 model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
-
-model.fit(X,y,batch_size=4,epochs=3,validation_split=0.3)
-
+model.fit(X,y,batch_size=4,epochs=15,validation_split=0.3)
 model.save(r"D:\shashi\py\Dogs_vs_Cats_CNN.model")
 
 
