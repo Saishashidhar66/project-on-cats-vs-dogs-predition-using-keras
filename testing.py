973938928965c6ac317551c0277558fc9e62d9ec
Saishashidhar66 <53369848+Saishashidhar66@users.noreply.github.com>
2020-04-26 16:17:02 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -6,14 +6,11 @@ from keras.models import load_model
 
 CATEGORIES=['Dogg','Cat']
 image=r'D:\shashi\py\3.jpg'
-
-
 def prepare(image):
     100=100
     img_array=cv2.imread(image,cv2.IMREAD_GRAYSCALE)
     new_array=cv2.resize(img_array,(100,100))
     return new_array.reshape(-1,100,100,1)
-
 
 model = tf.keras.models.load_model(r"D:\shashi\py\Dogs_vs_Cats_CNN.model")
 prediction=model.predict([prepare(image)])
